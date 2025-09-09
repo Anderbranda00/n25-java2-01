df216ad207c0c35cd324062504d55fb038bf0e76
anderson brandao pastorello <ander_brandao@hotmail.com>
2025-09-08 21:46:18 -0300

    Conteudo da Aaula 26

---
+++ Eclipse-Workspace/projeto_javafx_module/src/module-info.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module projeto_javafx_module {
+	requires javafx.graphics;
+	requires javafx.media;
+	requires javafx.controls;
+ opens br.com.senaisp.bauru.classes to javafx.graphics,
+                       javafx.media, javafx.controls;
+ 
+ 
+ 
+ 
+ 
+ 
+}
